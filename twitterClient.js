36529c1fcc16cdd914de174c21b129f5a24b16f6
Manuareraa <manuareraa@gmail.com>
2022-11-14 18:38:33 +0530

    Update

---
+++ twitterClient.js
@@ -0,0 +1,8 @@
+const { TwitterApi } = require("twitter-api-v2");
+
+module.exports = new TwitterApi({
+  appKey: "D0e4Ifb4NawvPw0BkBZRsPpKR",
+  appSecret: "YUVQqhz8Ed9ZcXuEcZIIPydAZhyrPx5UTgtiSSwIE9KN3BMbly",
+  accessToken: "791327931750506496-u74W3OGeE4UdTqbMvu9Rbqymwxaeir9",
+  accessSecret: "99JGsB77ZL3tmmSkxnrCoTw1kM9xYeXF5waIpmvXKHB92",
+});
