3baa083dc0d10269118454fba36cc68b7f5c2fc4
Manuareraa <manuareraa@gmail.com>
2022-11-16 23:16:57 +0530

    Update

--- controllers/addTasks.js
+++ controllers/addTasks.js
@@ -10,16 +10,12 @@ exports.addQuestion = async (req, res) => {
     { questions: req.body.questions },
     (err, doc) => {
       if (err) {
-        res.status(500).send({
-          message: err,
-        });
+        res.status(500).send({});
         console.log("Error occured while uploading: ", err);
         return;
       } else {
         console.log("Successfully uploaded the file")
-        res.status(200).send({
-          message: "Question Added successfully",
-        });
+        res.status(200).send({});
       }
     }
   );
